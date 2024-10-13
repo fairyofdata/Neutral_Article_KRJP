e76eb2ce074225a24b84bc7d3978fddec1c47389
zvkqO <pinku03260707@gmail.com>
2024-10-13 23:10:25 +0900

    ,

--- interface/main_survey_introduce.py
+++ interface/main_survey_introduce.py
@@ -451,14 +451,14 @@ with start_page:
                 </head>
                 <body>
                 <div class="container">
-                    <p style="text-align:center;">환영합니다!</p>
+                    <p style="text-align:center; text-color:#0000;">환영합니다!</p>
                     <p>이 설문은 귀하의 <strong>ESG(환경, 사회, 지배구조)</strong> 투자 관점과 가치에 대한 이해를 돕기 위해 마련되었습니다. 
                         귀하의 선호도를 반영하여 보다 개인화된 투자 분석과 포트폴리오 제안을 제공하기 위해, 간단한 질문에 응답해 주세요.
                         귀하의 설문 결과를 반영한 ESG 평가로 보다 신뢰할 수 있는 투자 정보를 제공하며, 사회적 책임과 환경적 가치를 고려한 맞춤형 포트폴리오를 설계합니다.
                     </p>
-                    <h2 style="font-size:20px; text-align:center;">소요 시간</h2>
-                    <p style="text-align:center;">약 <strong>5분</strong> 정도 소요됩니다.</p>
-                    <p style="text-align:center;">여러분의 소중한 의견은 지속 가능한 투자의 중요한 지침이 됩니다. 지금 바로 설문을 시작해 주세요!</p>
+                    <h2 style="font-size:20px; text-align:center;text-color:#0000;">소요 시간</h2>
+                    <p style="text-align:center;text-color:#0000;">약 <strong>5분</strong> 정도 소요됩니다.</p>
+                    <p style="text-align:center;text-color:#0000;">여러분의 소중한 의견은 지속 가능한 투자의 중요한 지침이 됩니다. 지금 바로 설문을 시작해 주세요!</p>
                 </div>
                 </body>
                 </html>
